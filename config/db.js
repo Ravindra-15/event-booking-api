5bda92da8a48f9e4490c0568d8a9670dcfa9a24f
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-04-01 16:20:05 +0530

    railway mysql + dotenv + production config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
